a79182738a57c214e1eb1ba614a2e2ba95becb9e
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-21 15:21:16 +0200

    component that holds my skills

---
+++ src/component/skills.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './styles/style.css'
+
+
+const skills = () => {
+    return (
+        <ul id="skills-ul">
+            <li> Javascript</li>,
+            <li> PHP</li>,
+            <li> C </li>,
+            <li> TypeScript</li>,
+            <li> Node.js</li>,
+            <li> Express</li>,
+            <li> HTML</li>,
+            <li> CSS</li>,
+            <li> Bootstrap</li>,
+            <li> React.js</li>,
+            <li> Codeignitor</li>,
+            <li> Socket.io</li>,
+            <li>Mysql</li>,
+            <li>MongoDB</li>.
+        </ul>
+    )
+}
+
+export default skills;
