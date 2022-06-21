7a22e248378be06914f5d456542206b310db0e2c
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-21 15:19:31 +0200

    component that contains copyright

---
+++ src/component/copyright.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles/style.css';
+
+const copyright = () => {
+    return (
+        <p id="copyright">pacifiquem &#169; 2022</p>
+    )
+}
+
+export default copyright;
